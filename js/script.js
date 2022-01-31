182d2e1d6af524dd30af14a3a67bbe990608a7e3
Daniel Mason <danmasson0@gmail.com>
2022-01-30 20:03:48 -0500

    Added features to show gussed letters, update the word in progress, and check to see if player has won.

--- js/script.js
+++ js/script.js
@@ -57,5 +57,39 @@ const makeGuess = (guess) => {
   } else {
     guessedLetters.push(guess);
     console.log(guessedLetters);
+    showGuessedLetters();
+    updateWordInProgress(guessedLetters);
+  }
+};
+
+const showGuessedLetters = () => {
+  playerGuesses.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    playerGuesses.append(li);
+  }
+};
+
+const updateWordInProgress = (guessedLetters) => {
+  let wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const showWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      showWord.push(letter.toUpperCase());
+    } else {
+      showWord.push("●");
+    }
+  }
+  console.log(showWord);
+  wordInProgress.innerText = showWord.join("");
+  checkWin();
+};
+
+const checkWin = () => {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
   }
 };
